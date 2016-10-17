65f3fc3f5e611d80f8d84c219bad45e145e04acc
ShaEemi <sharon.colin@eemi.com>
2016-10-17 16:59:53 +0200

    creation du fichier plugin

---
+++ marketfb.php
@@ -0,0 +1,18 @@
+<?php
+/**
+ *  Plugin Name : Facebook Market Like
+ *  Description : Facebook Market Like is a plugin that allows companies to display a widget with the most beloved items facebook
+ *  Version     : 0.1
+ *  Author      : Sharon Colin
+ */
+
+/**
+ *  Translation Function
+ */
+
+load_plugin_textdomain( 'marketFb', false, plugin_basename( dirname( __FILE__ ) ) . '/languages' );
+
+/**
+ *  Functions
+ */
+
