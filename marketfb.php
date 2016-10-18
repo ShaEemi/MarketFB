79e796f738b09df8559f3ad3db7c96dcda3c430b
ShaEemi <sharon.colin@eemi.com>
2016-10-18 11:44:25 +0200

    add dossier api fb

--- marketfb.php
+++ marketfb.php
@@ -1,11 +1,11 @@
 <?php
-/**
- *  Plugin Name : Facebook Market Like
- *  Description : Facebook Market Like is a plugin that allows companies to display a widget with the most beloved items facebook
- *  Version     : 0.1
- *  Author      : Sharon Colin
+/*
+ *  Plugin Name: Facebook Market Like
+ *  Description: Facebook Market Like is a plugin that allows companies to display a widget with the most beloved items facebook
+ *  Version: 0.1
+ *  Author: Sharon Colin
  */
-
+require_once __DIR__ . '/facebook-sdk-v5/src/Facebook/autoload.php';
 /**
  *  Translation Function
  */
